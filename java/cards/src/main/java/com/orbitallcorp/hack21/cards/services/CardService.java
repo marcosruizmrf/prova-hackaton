6b766b658955ca82f3ada6613b5e20b05ecf0695
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-01-23 11:11:22 -0300

    Implementação API's em Java

---
+++ java/cards/src/main/java/com/orbitallcorp/hack21/cards/services/CardService.java
@@ -0,0 +1,29 @@
+package com.orbitallcorp.hack21.cards.services;
+
+import com.orbitallcorp.hack21.cards.domains.Card;
+import com.orbitallcorp.hack21.cards.repositories.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CardService {
+    @Autowired
+    private CardRepository cardRepository;
+
+    public Card save(Card card) {
+        return cardRepository.save((card));
+    }
+
+    public List<Card> findAll() {
+
+        List<Card> cards = new ArrayList<Card>();
+
+        for (Card card : (List<Card>) cardRepository.findAll()) {
+            cards.add(card);
+        }
+        return cards;
+    }
+}
