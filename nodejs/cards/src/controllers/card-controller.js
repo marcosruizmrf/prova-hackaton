795e385fda57082ed3620ede260559c7d3c3c065
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-01-23 14:40:10 -0300

    Implementação de API's em Node.JS corrigida

--- nodejs/cards/src/controllers/card-controller.js
+++ nodejs/cards/src/controllers/card-controller.js
@@ -2,7 +2,7 @@ const neDB = require('../configurations/database')
 const api = {}
 
 api.findAll = (request, response) => {
-    neDB.find({}).sort({ name: 1 }).exec((exception, cards) => {
+    neDB.find({exception : 'cards'}).sort({ name: 1 }).exec((exception, cards) => {
         if (exception) {
             const setence = 'Deu ruim na tentativa de listar todos os cards!'
             console.log(setence, exception)
@@ -17,7 +17,7 @@ api.save = (request, response) => {
     const canonical = request.body
     neDB.insert(canonical, (exception, card) => {
         if (exception) {
-            const setence = 'Deu ruim na tentativa de listar todos os customers!'
+            const setence = 'Deu ruim na tentativa de listar todos os cards!'
             console.log(setence, exception)
             response.status(exception.status | 400)
             response.json({ 'mensagem': setence })
